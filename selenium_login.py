11c078db4a5985fe6497d1d951b644f4c1686d18
mini <mini3575445@gmail.com>
2025-02-17 21:37:53 +0800

    移除CreateToken

--- selenium_login.py
+++ selenium_login.py
@@ -55,30 +55,11 @@ def Get_listURL_simple() -> None:      #listStockId 股票代號string list, 網
     
 def Get_listURL_capitalim(): #listStockId 股票代號string list
     def Get_capitalim_Search_AuthToken():   
-        def CreateJWT():
-            header = {
-                "alg": "HS256",
-                "typ": "JWT"
-            }
-            current_time = datetime.datetime.now(datetime.timezone.utc)
-            payload = {
-                "id": "FrontWeb",
-                "nbf": int(current_time.timestamp()),  # JWT 生效時間（現在時間）
-                "exp": int((current_time + datetime.timedelta(days=1)).timestamp()),  # JWT 過期時間（1天後）
-                "iat": int(current_time.timestamp())  # JWT 發行時間（現在時間）
-            }
-            print(int(current_time.timestamp()))
-            # 生成 JWT
-            jwt_token = jwt.encode(payload, '', algorithm="none", headers=header)
-            print("生成的 JWT：", jwt_token)
-
         
-        requestToken = CreateJWT()
         url = "https://www.capitalim.com.tw/cmsapi/newcms/api/Users/web/login"
         headers = {
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Authorization": f"Bearer {requestToken}",
             "Connection": "keep-alive",
             "Cookie": "_ga=GA1.1.636061187.1720150588; G_ENABLED_IDPS=google; ASP.NET_SessionId=zquvpymlsiipkgzz5kidb3ra; TS01713f4a=01bdf56592d9ae36031d31ad481619862e7be2ddd6a10546352de49c11327aff05009eaaeb87545521941a9f1e5e48ccdf3ad15edf; _ga_HL4YRJH76V=GS1.1.1720591844.12.0.1720591852.52.0.0; TSaf2f34c4027=08763a8acdab2000d7ffd22b12d51b20c850ed2249f117f12c28454d1e1ce13f865c1d6ccc2f1b7508a00cdf47113000ebc46a7435ff9a68bf164bc5379800e6059efbe526022fc44b91a53262aa6f55ad0a22d15e6c238916da78bc4eb0bdd4",
             "Referer": "https://www.capitalim.com.tw/newsite/research-report/shares/14720;page=1",
@@ -94,6 +75,7 @@ def Get_listURL_capitalim(): #listStockId 股票代號string list
             "userId": "FrontWeb",
             "password": "FrontWeb888"
         }
+        
         response = requests.post(url, headers=headers, json=payload)
         # 檢查請求是否成功
         if response.status_code == 200:
